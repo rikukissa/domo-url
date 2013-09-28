96bd1c2e8dc3f47e540c43a291fe7402534fb9ed
Riku Rouvila <riku.rouvila@gmail.com>
2013-09-28 11:19:28 +0300

    initial push

---
+++ index.js
@@ -0,0 +1,34 @@
+var async, cheerio, request, urlRegex;
+
+async = require('async');
+
+cheerio = require('cheerio');
+
+request = require('request');
+
+urlRegex = /(\b(?:https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
+module.exports.init = function(domo) {
+  return domo.route(urlRegex, function(res) {
+    return async.map(res.message.match(urlRegex), request, function(err, responses) {
+      var titles;
+      if (err != null) {
+        return err;
+      }
+      titles = responses.filter(function(response) {
+        var $;
+        if (response.statusCode !== 200) {
+          return false;
+        }
+        $ = cheerio.load(response.body);
+        response.title = $('title').text();
+        return !!response.title;
+      }).map(function(response) {
+        return response.title;
+      }).join(', ');
+      if (!!titles) {
+        return domo.say(res.channel, titles);
+      }
+    });
+  });
+};
---
+++ src/index.coffee
@@ -0,0 +1,28 @@
+async   = require 'async'
+cheerio = require 'cheerio'
+request = require 'request'
+
+urlRegex = /(\b(?:https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig
+
+module.exports.init = (domo) ->
+
+  domo.route urlRegex, (res) ->
+
+    async.map res.message.match(urlRegex), request, (err, responses) ->
+      return err if err?
+
+      titles = responses.filter((response) ->
+        return false unless response.statusCode is 200
+
+        $ = cheerio.load(response.body)
+        response.title = $('title').text()
+
+        not not response.title
+
+      ).map((response) ->
+        return response.title
+
+      ).join(', ')
+
+      domo.say res.channel, titles if not not titles
+
